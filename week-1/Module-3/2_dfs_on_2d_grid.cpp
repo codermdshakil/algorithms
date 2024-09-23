2d0de9e401b538c08f6779c5bce80db234403bb9
codermdshakil <ahmedshakil0512@gmail.com>
2024-09-23 22:43:03 +0600

    Learn : take 2d grid input and print

---
+++ week-1/Module-3/2_dfs_on_2d_grid.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+char a[20][20];
+
+int main()
+{
+
+    int n, m;
+    cin >> n >> m;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cin >> a[i][j];
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cout << a[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
