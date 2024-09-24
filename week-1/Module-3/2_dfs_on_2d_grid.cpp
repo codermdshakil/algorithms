39884a5bedf888f7d3449cb47c0e9df647d39be9
codermdshakil <ahmedshakil0512@gmail.com>
2024-09-24 22:17:52 +0600

    Learn : Implement DFS on 2D grid

--- week-1/Module-3/2_dfs_on_2d_grid.cpp
+++ week-1/Module-3/2_dfs_on_2d_grid.cpp
@@ -2,11 +2,46 @@
 using namespace std;
 
 char a[20][20];
+bool visit[20][20];
+
+// to move right left up down
+vector<pair<int, int>> d = {{0, 1}, {0, -1}, {-1, 0}, {1, 0}};
+
+int n, m;
+
+// create a function to check source index valid or not
+bool valid(int ci, int cj)
+{
+    if (ci < 0 || ci >= n || cj < 0 || cj >= m)
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
+void dfs(int si, int sj)
+{
+    cout << si << " " << sj << endl;
+    visit[si][sj] = true;
+
+    // take source childrens right, left, up, down
+    for (int i = 0; i < 4; i++)
+    {
+        int ci = si + d[i].first;
+        int cj = sj + d[i].second;
+        if (valid(ci, cj) == true && visit[ci][cj] == false)
+        {
+            dfs(ci, cj);
+            visit[ci][cj] = true;
+        }
+    }
+}
 
 int main()
 {
 
-    int n, m;
     cin >> n >> m;
     for (int i = 0; i < n; i++)
     {
@@ -15,14 +50,14 @@ int main()
             cin >> a[i][j];
         }
     }
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < m; j++)
-        {
-            cout << a[i][j] << " ";
-        }
-        cout << endl;
-    }
+
+    // take source input
+    int si, sj;
+    cin >> si >> sj;
+
+    memset(visit, false, sizeof(visit));
+
+    dfs(si, sj);
 
     return 0;
 }
