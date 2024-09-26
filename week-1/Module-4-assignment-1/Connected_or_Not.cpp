9ed160d0cef288b6fd4788f87518e5905b851195
codermdshakil <ahmedshakil0512@gmail.com>
2024-09-26 22:59:00 +0600

    Problem : assignment not complete

--- week-1/Module-4-assignment-1/Connected_or_Not.cpp
+++ week-1/Module-4-assignment-1/Connected_or_Not.cpp
@@ -1,72 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> v[1005];
-
-bool visit[1005];
-
-// bsf travere
-void bsf(int src, int des)
-{
-    queue<pair<int, int>> q;
-    q.push({src, 0});
-    visit[src] = true;
-    bool paise = false;
-
-    while (!q.empty())
-    {
-        pair<int, int> p = q.front();
-        q.pop();
-        int parent = p.first;
-        int level = p.second;
-
-        if (parent == des)
-        {
-            if (level == 1 || level == 0)
-            {
-                paise = true;
-            }
-        }
-
-        for (auto child : v[parent])
-        {
-            if (visit[child] == false)
-            {
-                q.push({child, level + 1});
-                visit[child] = true;
-            }
-        }
-    }
-    if (paise == false)
-    {
-        cout << "NO" << endl;
-    }
-    else
-    {
-
-        cout << "YES" << endl;
-    }
-}
-
 int main()
 {
+
     int n, e;
     cin >> n >> e;
+
+    int adj_mat[n][n];
+
+    memset(adj_mat, 0, sizeof(adj_mat));
 
     while (e--)
     {
         int a, b;
         cin >> a >> b;
-        v[a].push_back(b);
+        adj_mat[a][b] = 1;
     }
+
     int q;
     cin >> q;
     while (q--)
     {
-        int src, des;
-        cin >> src >> des;
-        memset(visit, false, sizeof(visit));
-        bsf(src, des);
+        int a, b;
+        cin >> a >> b;
+        if (adj_mat[a][b] == 1)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
     }
 
     return 0;
