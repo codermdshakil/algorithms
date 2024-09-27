91b87fd1a8e1d8105b9548757f3256259c0a1ff5
codermdshakil <ahmedshakil0512@gmail.com>
2024-09-28 03:22:43 +0600

    Problem Solved : Assignment 1:3 Can go

--- week-1/Module-4-assignment-1/Can_Go.cpp
+++ week-1/Module-4-assignment-1/Can_Go.cpp
@@ -1,30 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// graph to store matrix elements
 char graph[1001][1001];
-
-// to check visited or not
 bool visit[1001][1001];
-
-// store input of row and col
 int n, m;
 
-// valid index or not check function
+// Valid index check function
 bool valid(int ci, int cj)
 {
-    if (ci >= 0 && ci < n && cj >= 0 && cj < m && graph[ci][cj] == '.')
+    if (ci >= 0 && ci < n && cj >= 0 && cj < m)
     {
-        return true;
+        return (graph[ci][cj] == '.' || graph[ci][cj] == 'A' || graph[ci][cj] == 'B');
     }
-    else if (graph[ci][cj] == 'A' || graph[ci][cj] == 'B')
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
+    return false;
 }
 
 // to move x wise
@@ -33,18 +21,19 @@ int dx[4] = {-1, 0, 1, 0};
 // to move y wise
 int dy[4] = {0, 1, 0, -1};
 
+// dfs traversal funcition
 void dfs(int si, int sj)
 {
     visit[si][sj] = true;
 
-    // get children
+    // get childrens
     for (int i = 0; i < 4; i++)
     {
-        //  childrens
         int ci = si + dx[i];
         int cj = sj + dy[i];
 
-        if (visit[ci][cj] == false && valid(ci, cj))
+        // check valid and not visited
+        if (valid(ci, cj) && !visit[ci][cj])
         {
             dfs(ci, cj);
             visit[ci][cj] = true;
@@ -54,21 +43,37 @@ void dfs(int si, int sj)
 
 int main()
 {
-
     cin >> n >> m;
 
+    // store room a and b
+    pair<int, int> roomA, roomB;
+
+    // Input the grid
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < m; j++)
         {
             cin >> graph[i][j];
+
+            // input room A
+            if (graph[i][j] == 'A')
+            {
+                roomA = {i, j};
+            }
+
+            // input room B
+            if (graph[i][j] == 'B')
+            {
+                roomB = {i, j};
+            }
         }
     }
 
-    dfs(1, 2); // source node
+    dfs(roomA.first, roomA.second);
 
-    // check can go or not
-    if (visit[2][6] == true)
+    // Print output
+
+    if (visit[roomB.first][roomB.second])
     {
         cout << "YES" << endl;
     }
