7c9174a82a612c22177abc02fefa093e920e6278
codermdshakil <ahmedshakil0512@gmail.com>
2024-09-27 21:47:57 +0600

    Learn : Country counting using DFS [clear code]

--- week-1/Module-3.5/week-1-2/1_counting_countries_by_DFS.cpp
+++ week-1/Module-3.5/week-1-2/1_counting_countries_by_DFS.cpp
@@ -1,13 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// graph to store matrix elements
 char graph[1001][1001];
+
+// visited mat to check item visited or not
 bool visit[1001][1001];
+
+// to move x wais
+int dx[4] = {-1, 0, 1, 0};
+
+// to move y wais
+int dy[4] = {0, 1, 0, -1};
+
+// take row and col input
 int n, m;
 
-int dx[4] = {-1, 0, 1, 0}; // row move
-int dy[4] = {0, 1, 0, -1}; // coloum move
-
+// check index are valid or not
 bool valid(int ci, int cj)
 {
     if (ci >= 0 && ci < n && cj >= 0 && cj < m && graph[ci][cj] == '.')
@@ -20,28 +29,31 @@ bool valid(int ci, int cj)
     }
 }
 
+// dfs traversal
 void dfs(int si, int sj)
 {
     visit[si][sj] = true;
+
     for (int i = 0; i < 4; i++)
     {
         int ci = si + dx[i]; // children row
-        int cj = sj + dy[i]; // children colum
+        int cj = sj + dy[i]; // children col
 
+        // check valid index and visit false
         if (visit[ci][cj] == false && valid(ci, cj) == true)
         {
-            visit[ci][cj] = true;
             dfs(ci, cj);
+            visit[ci][cj] = true;
         }
     }
 }
 
 int main()
 {
-    // . means Country
-    // ~ means Water
-
+    // take row and col input
     cin >> n >> m;
+
+    // take input of matrix elements
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < m; j++)
@@ -50,22 +62,24 @@ int main()
         }
     }
 
-    // country_counting
-    int countery_count = 0;
+    // store count of country
+    int country_count = 0;
+
+    // country count logic
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < m; j++)
         {
-            // if country using '.' and matrix elements are not visited
+            // check country has and not visited
             if (graph[i][j] == '.' && visit[i][j] == false)
             {
                 dfs(i, j);
-                countery_count++;
+                country_count++;
             }
         }
     }
 
-    cout << countery_count << endl;
+    cout << country_count << endl;
 
     return 0;
 }
