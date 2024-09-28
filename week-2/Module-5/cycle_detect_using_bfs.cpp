7f2672e115486748ef45fd9457be2d73a18f2864
codermdshakil <ahmedshakil0512@gmail.com>
2024-09-28 22:31:38 +0600

    Learn : Cycle detect on underected graph using BFS

---
+++ week-2/Module-5/cycle_detect_using_bfs.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 1e5 + 5;
+vector<int> adj[N];
+
+bool visit[N];
+int parentArray[N];
+
+bool ans;
+
+void bfs(int s)
+{
+    queue<int> q;
+    q.push(s);
+    visit[s] = true;
+
+    while (!q.empty())
+    {
+        int parent = q.front();
+        q.pop();
+
+        // cout << parent << " ";
+
+        for (int child : adj[parent])
+        {
+            // cycle deteck logic
+
+            if (visit[child] == true && parentArray[parent] != child)
+            {
+                ans = true;
+            }
+            if (visit[child] == false)
+            {
+                q.push(child);
+                parentArray[child] = parent;
+                visit[child] = true;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int n, e;
+    cin >> n >> e;
+
+    while (e--)
+    {
+        int a, b;
+        cin >> a >> b;
+        adj[a].push_back(b);
+        adj[b].push_back(a);
+    }
+
+    memset(visit, false, sizeof(visit));
+    memset(parentArray, -1, sizeof(parentArray));
+
+    // inisially ans false
+    ans = false;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (!visit[i])
+        {
+            bfs(i);
+        }
+    }
+
+    if(ans){
+        cout << "Cycle Found" << endl;
+    }
+    else{
+        
+        cout << "Cycle not Found" << endl;
+    }
+
+    return 0;
+}
