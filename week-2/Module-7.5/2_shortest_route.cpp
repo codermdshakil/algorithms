9cd4e13f9134386cd3806c8bc7cf2b11994723b8
codermdshakil <ahmedshakil0512@gmail.com>
2024-10-05 14:59:33 +0600

    Resolved : Corner case in Shortest route

--- week-2/Module-7.5/2_shortest_route.cpp
+++ week-2/Module-7.5/2_shortest_route.cpp
@@ -1,72 +1,60 @@
+// https://cses.fi/problemset/task/1672
+
 #include <bits/stdc++.h>
 #define ll long long int
-// max 10^18
 const long long int INF = 1e18 + 5;
 using namespace std;
-
 int main()
 {
     ll n, e, q;
     cin >> n >> e >> q;
-
-    // adjacency matrix
     ll adj[n + 5][n + 5];
-
-    // adjacency matrix default all value infinite or INT_MAX
     for (int i = 1; i <= n; i++)
     {
         for (int j = 1; j <= n; j++)
         {
             adj[i][j] = INF;
             if (i == j)
-            {
                 adj[i][j] = 0;
-            }
         }
     }
-
-    // take edge input
     while (e--)
     {
-        int a, b, c;
+        ll a, b, c;
         cin >> a >> b >> c;
-        adj[a][b] = c;
-        adj[b][a] = c; // if underected
+        // first way to handle corner case
+        adj[a][b] = min(adj[a][b], c);
+        adj[b][a] = min(adj[b][a], c);
+
+        // // second way to handle corner case
+        // if (c < adj[a][b])
+        // {
+        //     adj[a][b] = c;
+        //     adj[b][a] = c;
+        // }
     }
-
-    // floyd warshall logic
-    // time complexity - O(v^3)
-    // any node to any node shortest distance
-
     for (int k = 1; k <= n; k++)
     {
         for (int i = 1; i <= n; i++)
         {
             for (int j = 1; j <= n; j++)
             {
-                // logic of floyd warshall
                 if (adj[i][k] + adj[k][j] < adj[i][j])
                 {
-                    // path relax
                     adj[i][j] = adj[i][k] + adj[k][j];
                 }
             }
         }
     }
 
-    // output   
     while (q--)
     {
         int s, d;
         cin >> s >> d;
         if (adj[s][d] == INF)
-        {
             cout << -1 << endl;
-        }
         else
-        {
             cout << adj[s][d] << endl;
-        }
     }
 
     return 0;
