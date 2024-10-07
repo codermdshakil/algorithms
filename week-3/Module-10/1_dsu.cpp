451d350c2e31f49675943aa9641d97854dcaadd2
codermdshakil <ahmedshakil0512@gmail.com>
2024-10-08 00:29:24 +0600

    Learn : DSU Find function to find the leader

---
+++ week-3/Module-10/1_dsu.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 1e5 + 5;
+
+int par[N];
+
+// inisially parent all values are -1 that means everyone is Leader
+void dsu_inisialize(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        par[i] = -1;
+    }
+    par[1] = 3;
+    par[2] = 1;
+}
+
+// find who is leader and return leader
+int find(int node)
+{
+    if (par[node] == -1)
+    {
+        return node;
+    }
+    return find(par[node]);
+}
+
+int main()
+{
+    dsu_inisialize(4);
+    cout << find(0) << endl;
+    cout << find(1) << endl;
+    cout << find(2) << endl;
+    cout << find(3) << endl;
+
+    return 0;
+}
