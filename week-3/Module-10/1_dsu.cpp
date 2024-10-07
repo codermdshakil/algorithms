c2e5c5f22e8178d80176400ace7b4cd6b8b2ef35
codermdshakil <ahmedshakil0512@gmail.com>
2024-10-08 01:23:08 +0600

    Learn : dsu union here we sacrifice group A for learning purpose

--- week-3/Module-10/1_dsu.cpp
+++ week-3/Module-10/1_dsu.cpp
@@ -12,11 +12,13 @@ void dsu_inisialize(int n)
     {
         par[i] = -1;
     }
-    par[1] = 3;
-    par[2] = 1;
+    par[1] = 2;
+    par[2] = 3;
+    par[5] = 6;
+    par[6] = 7;
 }
 
-// find who is leader and return leader 
+// find who is leader and return leader
 // time Complexity - O(logN) better then O(N)
 
 int dsu_find(int node)
@@ -26,19 +28,33 @@ int dsu_find(int node)
         return node;
     }
 
-    // Path compression 
+    // Path compression
     int leader = dsu_find(par[node]);
     par[node] = leader;
     return leader;
 }
 
+void dsu_union(int node1, int node2)
+{
+    int leaderA = dsu_find(node1); // 3
+    int leaderB = dsu_find(node2); // 7
+
+    // leaderA sacrifice his leaderness and leaderA parent is leaderB. Altemately both node leader is leaderB
+    par[leaderA] = leaderB;
+}
+
 int main()
 {
-    dsu_inisialize(4);
-    cout << dsu_find(0) << endl;
+    dsu_inisialize(8);
+    dsu_union(1, 5); // sent here two nodes root value
+
     cout << dsu_find(1) << endl;
     cout << dsu_find(2) << endl;
     cout << dsu_find(3) << endl;
+    cout << dsu_find(5) << endl;
+    cout << dsu_find(6) << endl;
+    cout << dsu_find(7) << endl;
+    
 
     return 0;
 }
