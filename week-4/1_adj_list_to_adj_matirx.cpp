78410d18980c392422722851ac7632c5cfd3499e
codermdshakil <ahmedshakil0512@gmail.com>
2024-10-11 16:57:32 +0600

    Learn : Convert adjacency list to adjacency matrix

---
+++ week-4/1_adj_list_to_adj_matirx.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void convert_adjList_to_adjMatrix(int n, vector<int> adj[])
+{
+    // 1. create adjacency matrix using size n
+    int adj_matrix[n][n];
+
+    // inisial all value 0
+    memset(adj_matrix, 0, sizeof(adj_matrix));
+    for (int i = 0; i < n; i++)
+    {
+        // get childrens
+        for (int child : adj[i])
+        {
+            adj_matrix[i][child] = 1;
+        }
+    }
+
+    // print matrix
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << adj_matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    int n, e;
+    cin >> n >> e;
+    vector<int> v[n];
+    while (e--)
+    {
+        int a, b;
+        cin >> a >> b;
+        v[a].push_back(b);
+        v[b].push_back(a);
+    }
+
+    convert_adjList_to_adjMatrix(n, v);
+
+    return 0;
+}
