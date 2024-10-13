7c1e08f28f731ec2e1d3b7ee8ce10a220f16b2dd
codermdshakil <ahmedshakil0512@gmail.com>
2024-10-14 01:13:56 +0600

    Learn : Fibonacci

---
+++ week-5/Module-16/2_fibonacci.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int fibonacci(int n)
+{
+    // basecase
+    if (n == 0 || n == 1)
+    {
+        return n;
+    }
+    int ans = fibonacci(n - 1) + fibonacci(n - 2);
+    return ans;
+}
+
+int main()
+{
+
+    int n;
+    cin >> n;
+
+    cout << fibonacci(n) << endl;
+
+    return 0;
+}
