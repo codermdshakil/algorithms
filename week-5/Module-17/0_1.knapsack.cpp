7217e9ec24cd4f5c466df2bec80f07f31fe8be31
codermdshakil <ahmedshakil0512@gmail.com>
2024-10-14 19:28:13 +0600

    Learn : Knapsack

---
+++ week-5/Module-17/0_1.knapsack.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// knapsack implementaion
+int knapsack(int n, int weight[], int value[], int W)
+{
+    // base case
+    if (n < 0 || W == 0)
+        return 0;
+
+    if (weight[n] <= W)
+    {
+        // doita option
+        // 1. niye dekhbo
+        // 2 na niye dekhbo
+        int op1 = knapsack(n - 1, weight, value, W - weight[n]) + value[n];
+        int op2 = knapsack(n - 1, weight, value, W);
+        return max(op1, op2);
+    }
+
+    else
+    {
+        // ektai option
+        // na niyei e dekhte hobe
+        int op2 = knapsack(n - 1, weight, value, W);
+        return op2;
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int weight[n], value[n];
+
+    // take weight input
+    for (int i = 0; i < n; i++)
+    {
+        cin >> weight[i];
+    }
+
+    // take weight input
+    for (int i = 0; i < n; i++)
+    {
+        cin >> value[i];
+    }
+
+    // total of knapsack
+    int W;
+    cin >> W;
+
+    // n-1 means last index ta dise
+    cout << knapsack(n - 1, weight, value, W) << endl;
+
+    return 0;
+}
