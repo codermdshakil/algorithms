86e29e3d1ef060ba6043cab222c2239b17e5bf93
codermdshakil <ahmedshakil0512@gmail.com>
2024-10-15 22:50:26 +0600

    Learn : fibonacci using DP

--- week-5/Module-17.5/week-5-1/1_fibo.cpp
+++ week-5/Module-17.5/week-5-1/1_fibo.cpp
@@ -1,21 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int dp[100005];
 
-int fibo(int n){
+int fibo(int n)
+{
     // basecase
-    if(n<2){
+    if (n < 2)
+    {
         return n;
     }
-    return fibo(n-1) + fibo(n-2);
+
+    if(dp[n] != -1){
+        return dp[n];
+    }
+
+    dp[n] = fibo(n - 1) + fibo(n - 2);
+    return dp[n];
 }
 
-int main(){
+int main()
+{
+
+    // initialize dp with -1
+    memset(dp, -1, sizeof(dp));
 
     int n;
-    cin>>n;
+    cin >> n;
 
     cout << fibo(n) << endl;
-    
+
     return 0;
 }
