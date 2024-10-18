45b288562d7fca97bd4579940c287627a4d56c60
codermdshakil <ahmedshakil0512@gmail.com>
2024-10-18 08:43:31 +0600

    Learn : Subset Sum using DP  Down -> Up (Loop) approch to check possible or not sum

---
+++ week-5/Module-18/2_subset_sum_down_up.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    // create a array to store values
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    // take sum input
+    int sum;
+    cin >> sum;
+
+    // create a dp array using n and sum
+    bool dp[n + 1][sum + 1];
+
+    // initial  0 row and 0 col value true
+    dp[0][0] = true;
+    // inisially 0 row and col 0 value false
+    for (int i = 1; i <= sum; i++)
+    {
+        dp[0][i] = false;
+    }
+
+    // DP down -> up approch using loop
+    for (int i = 1; i <= n; i++)
+    {
+
+        for (int j = 0; j <= sum; j++)
+        {
+            if (arr[i - 1] <= j)
+            {
+                // doita option
+                bool op1 = dp[i - 1][j - arr[i - 1]];
+                bool op2 = dp[i - 1][j];
+                dp[i][j] = op1 || op2;
+            }
+            else
+            {
+                // ektai option
+                dp[i][j] = dp[i - 1][j];
+            }
+        }
+    }
+
+    if (dp[n][sum])
+    {
+        cout << "YES\n";
+    }
+    else
+    {
+        cout << "NO\n";
+    }
+
+    return 0;
+}
