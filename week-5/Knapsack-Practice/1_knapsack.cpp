ce8966acb35452235d165efa1991b32cb67af0c5
codermdshakil <ahmedshakil0512@gmail.com>
2024-10-21 20:41:53 +0600

    Learn : Knapsack not optimaized

---
+++ week-5/Knapsack-Practice/1_knapsack.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int knapsack(int n, int weight[], int value[], int W)
+{
+    // basecase
+    if (n < 0 || W == 0)
+    {
+        return 0;
+    }
+
+    if (weight[n] <= W)
+    {
+        // two option
+        int op1 = knapsack(n - 1, weight, value, W - weight[n]) + value[n];
+        int op2 = knapsack(n - 1, weight, value, W);
+        return max(op1, op2);
+    }
+    else
+    {
+        // option ekta e
+        int op2 = knapsack(n - 1, weight, value, W);
+    }
+}
+
+int main()
+{
+
+    int n;
+    cin >> n;
+    int weight[n], value[n];
+    // take weight input
+    for (int i = 0; i < n; i++)
+    {
+        cin >> weight[i];
+    }
+    // take value input
+    for (int i = 0; i < n; i++)
+    {
+        cin >> value[i];
+    }
+
+    int W;
+    cin >> W;
+
+    cout << knapsack(n - 1, weight, value, W);
+
+    return 0;
+}
